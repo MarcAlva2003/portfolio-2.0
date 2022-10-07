25a677c31810dbf08a58811d274207603e33fcc2
Marcos Alvarez <alvarez2003marcos@gamil.com>
2022-10-06 21:14:14 -0300

    Add: section 2 desktop [In progress]

---
+++ app/swiper.js
@@ -0,0 +1,19 @@
+var swiper = new Swiper(".mySwiper", {
+  speed: 1000,
+  parallax: true,
+  spaceBetween: 70
+});
+// pagination-button-disabled
+const prevSlideButton = document.getElementById('pagination-button-prev')
+const nextSlideButton = document.getElementById('pagination-button-next')
+
+prevSlideButton.addEventListener('click', ()=>{
+  nextSlideButton.classList.remove('pagination-button-disabled');
+  swiper.slideTo(swiper.activeIndex - 1);
+  swiper.activeIndex === 0 ? prevSlideButton.classList.add('pagination-button-disabled') : '';
+})
+nextSlideButton.addEventListener('click', ()=>{
+  prevSlideButton.classList.remove('pagination-button-disabled');
+  swiper.slideTo(swiper.activeIndex + 1);
+  swiper.activeIndex + 1 === swiper.slides.length ? nextSlideButton.classList.add('pagination-button-disabled') : '';
+})
