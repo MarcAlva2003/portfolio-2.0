2fb084a81b9bb8bc4a0adcf1cc208b4aefeddc2f
Marcos Alvarez <alvarez2003marcos@gamil.com>
2022-10-08 12:41:54 -0300

    Add: akills height transition

--- app/section-3.js
+++ app/section-3.js
@@ -1,15 +1,32 @@
 // skill-row-bottom skill-row-bottom-open
 
 const skillRows = document.querySelectorAll('.skill-row');
-let openSkillIndex = -1;
+const skillDesc = document.querySelectorAll('.skill-description');
+const skillText = document.querySelectorAll('.skill-row-bottom')
+
+
+// let openSkillIndex = -1;
+// const openSkill = (id) => {
+//   skillRows.forEach((item) => item.classList.remove('skill-row-open'));
+//   skillText.forEach((item) => {item.style["height"] = "0"})
+//   if(openSkillIndex !== id){
+//     skillText[id].style["height"] = `${skillDesc[id].offsetHeight + 20}px`
+//     openSkillIndex = id;
+//   } else {
+//     openSkillIndex = -1;
+//   }
+// }
+
+let skillsOpen = [];
 
 const openSkill = (id) => {
-  skillRows.forEach((item) => item.classList.remove('skill-row-open'));
-  if(openSkillIndex !== id){
+  if(!skillsOpen.includes(id)){
     skillRows[id].classList.add('skill-row-open');
-    openSkillIndex = id;
+    skillText[id].style["height"] = `${skillDesc[id].offsetHeight + 20}px`
+    skillsOpen.push(id);
   } else {
-    openSkillIndex = -1;
-
+    skillRows[id].classList.remove('skill-row-open');
+    skillText[id].style["height"] = `0`
+    skillsOpen = skillsOpen.filter(i => i !== id);
   }
 }
