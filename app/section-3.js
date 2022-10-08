5de4ed1e428e04f90fefb233e3c7e37ba272f01f
Marcos Alvarez <alvarez2003marcos@gamil.com>
2022-10-08 12:47:29 -0300

    Close skill when other one is open

--- app/section-3.js
+++ app/section-3.js
@@ -5,28 +5,28 @@ const skillDesc = document.querySelectorAll('.skill-description');
 const skillText = document.querySelectorAll('.skill-row-bottom')
 
 
-// let openSkillIndex = -1;
-// const openSkill = (id) => {
-//   skillRows.forEach((item) => item.classList.remove('skill-row-open'));
-//   skillText.forEach((item) => {item.style["height"] = "0"})
-//   if(openSkillIndex !== id){
-//     skillText[id].style["height"] = `${skillDesc[id].offsetHeight + 20}px`
-//     openSkillIndex = id;
-//   } else {
-//     openSkillIndex = -1;
-//   }
-// }
-
-let skillsOpen = [];
-
+let openSkillIndex = -1;
 const openSkill = (id) => {
-  if(!skillsOpen.includes(id)){
-    skillRows[id].classList.add('skill-row-open');
+  skillRows.forEach((item) => item.classList.remove('skill-row-open'));
+  skillText.forEach((item) => {item.style["height"] = "0"})
+  if(openSkillIndex !== id){
     skillText[id].style["height"] = `${skillDesc[id].offsetHeight + 20}px`
-    skillsOpen.push(id);
+    openSkillIndex = id;
   } else {
-    skillRows[id].classList.remove('skill-row-open');
-    skillText[id].style["height"] = `0`
-    skillsOpen = skillsOpen.filter(i => i !== id);
+    openSkillIndex = -1;
   }
 }
+
+// let skillsOpen = [];
+
+// const openSkill = (id) => {
+//   if(!skillsOpen.includes(id)){
+//     skillRows[id].classList.add('skill-row-open');
+//     skillText[id].style["height"] = `${skillDesc[id].offsetHeight + 20}px`
+//     skillsOpen.push(id);
+//   } else {
+//     skillRows[id].classList.remove('skill-row-open');
+//     skillText[id].style["height"] = `0`
+//     skillsOpen = skillsOpen.filter(i => i !== id);
+//   }
+// }
